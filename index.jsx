ba3151a9730e7f97d995aacdbf24c2cf5e423c93
radubrehar <radu@jslog.com>
2014-10-15 15:18:33 +0300

    fix saturation problem

--- index.jsx
+++ index.jsx
@@ -4,12 +4,40 @@ require('./resources/style/index.styl')
 
 var React = require('react')
 var ColorPicker = require('./src/index')
-var HueSpectrum = require('./src/HueSpectrum')
-var SaturationSpectrum = require('./src/SaturationSpectrum')
 
 var colorUtils = require('./src/utils/color')
 
 var COLOR = 'magenta'
+var App = React.createClass({
+    displayName: 'App',
+    render: function(){
 
-React.renderComponent(<ColorPicker red='red'/>, document.body)
+        var onChange = function(event){
+            COLOR = event.target.value
+            this.setState({})
 
+        }.bind(this)
+
+        return React.DOM.div({},
+            ColorPicker({
+                // defaultValue: 'red',
+                value: COLOR,
+                onDrag: function(color){
+                    COLOR = color
+                    this.setState({})
+                }.bind(this),
+                style: {
+                    height: 400,
+                    width: 400,
+                    border: '1px solid red',
+                    margin: 10
+                }
+            }),
+            <div style ={{background: COLOR + '', width: 80, height: 30}}>
+                {COLOR}
+            </div>
+        )
+    }
+})
+
+React.renderComponent(App(), document.getElementById('content'))
